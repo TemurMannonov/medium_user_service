48f11025a33390853eec44d7694d61f656bf225c
Temur Mannonov <temur.mannonov@zazmic.com>
2022-12-23 09:52:32 +0500

    update migration

--- migrations/2_create_permissions_table.up.sql
+++ migrations/2_create_permissions_table.up.sql
@@ -6,11 +6,20 @@ CREATE TABLE IF NOT EXISTS permissions(
     UNIQUE(user_type, resource, action)   
 );
 
+-- users
 INSERT INTO permissions(user_type, resource, action) VALUES ('superadmin', 'users', 'create');
 INSERT INTO permissions(user_type, resource, action) VALUES ('superadmin', 'users', 'update');
 INSERT INTO permissions(user_type, resource, action) VALUES ('superadmin', 'users', 'delete');
-INSERT INTO permissions(user_type, resource, action) VALUES ('superadmin', 'categories', 'create');
-INSERT INTO permissions(user_type, resource, action) VALUES ('superadmin', 'posts', 'create');
-INSERT INTO permissions(user_type, resource, action) VALUES ('user', 'posts', 'create');
+
 INSERT INTO permissions(user_type, resource, action) VALUES ('user', 'users', 'update');
 INSERT INTO permissions(user_type, resource, action) VALUES ('user', 'users', 'delete');
+
+-- categories
+INSERT INTO permissions(user_type, resource, action) VALUES ('superadmin', 'categories', 'create');
+
+-- posts
+INSERT INTO permissions(user_type, resource, action) VALUES ('superadmin', 'posts', 'create');
+INSERT INTO permissions(user_type, resource, action) VALUES ('superadmin', 'posts', 'update');
+INSERT INTO permissions(user_type, resource, action) VALUES ('user', 'posts', 'create');
+INSERT INTO permissions(user_type, resource, action) VALUES ('user', 'posts', 'update');
+
